28f21b61ce0c7663fefb8bc71438ce0aefd1a075
Min-SungJo <123or@naver.com>
2022-11-17 22:31:53 +0900

    create User & UserDao

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -2,10 +2,7 @@ package springbook.user.dao;
 
 import springbook.user.domain.User;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException{
@@ -20,5 +17,34 @@ public class UserDao {
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
         ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+        ps.close();
+        conn.close();
     }
+
+    public User get(String id) throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://locallhost/springbook", "spring", "book"
+        );
+        PreparedStatement ps = conn.prepareStatement(
+                "select * from users where id =?"
+        );
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        conn.close();
+
+        return user;
+    }
+
 }
--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -11,13 +11,13 @@ public class User {
     public String getId(){
         return id;
     }
-    public void setId(){
+    public void setId(String id){
         this.id=id;
     }
     public String getName(){
         return name;
     }
-    public void setName(){
+    public void setName(String name){
         this.name=name;
     }
     public String getPassword(){
